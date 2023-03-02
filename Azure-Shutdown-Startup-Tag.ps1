e25e38d2f822babcebe0cdd24a2d49824cdbcab8
roconnor04 <roconnor04@gmail.com>
2023-03-02 14:09:44 +0000

    Update Azure-Shutdown-Startup-Tag.ps1

--- Azure-Shutdown-Startup-Tag.ps1
+++ Azure-Shutdown-Startup-Tag.ps1
@@ -1,12 +1,13 @@
 # Connect to Azure
 Connect-AzAccount -Identity
 
+
 # Get all Virtual Machines with the tag "ShutdownTime"
-$vmsShutdown = Get-AzVM | Where-Object {($_.Tags.Keys -contains "ShutdownTime") -and ($_.PowerState -eq 'VM running')}
-$vmsStartup = Get-AzVM | Where-Object {($_.Tags.Keys -contains "StartTime") -and ($_.PowerState -eq 'VM deallocated')}
+$vmsShutdown = Get-AzVM -status | Where-Object {($_.Tags.Keys -contains "ShutdownTime") -and ($_.PowerState -eq 'VM running')}
+$vmsStartup = Get-AzVM -status  | Where-Object {($_.Tags.Keys -contains "StartTime") -and ($_.PowerState -eq 'VM deallocated')}
 # Loop through each VM
 foreach ($vm in $vmsShutdown) {
-    # Get the value of the ShutdownTime tag
+    # Get the value of the ShutdownTime tagVM deallocated
     $shutdownTime = [DateTime]::ParseExact($vm.Tags["ShutdownTime"], "HH:mm", $null)
     #$shutdownTime = $vm.Tags["ShutdownTime"]
     
@@ -17,9 +18,12 @@ foreach ($vm in $vmsShutdown) {
     # Compare the value of the tag with the current time
     if ($timeDiff -le 80 -and $timeDiff -ge 0) {
         # Shut down the VM
-        Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
         Write-Output "Shutting down VM $($vm.Name) in $($vm.ResourceGroupName) because ShutdownTime tag value is $($shutdownTime)."
+        Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
+
     }
+
+}  
 
 foreach ($vm in $vmsStartup) {
     # Get the value of the ShutdownTime tag
@@ -33,8 +37,8 @@ foreach ($vm in $vmsStartup) {
     # Compare the value of the tag with the current time
     if ($timeDiff -le 60 -and $timeDiff -ge 0) {
         # Shut down the VM
-        Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
         Write-Output "Starting VM $($vm.Name) in $($vm.ResourceGroupName) because StartupTime tag value is $($startupTime)."
+        Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+
     }
-}
 }
