cbbbc776bd2086a370d6e9bb982528c03833647d
roconnor04 <roconnor04@gmail.com>
2023-03-02 09:05:17 +0000

    Add files via upload

---
+++ Azure-Shutdown-Startup-Tag.ps1
@@ -0,0 +1,40 @@
+# Connect to Azure
+Connect-AzAccount -Identity
+
+# Get all Virtual Machines with the tag "ShutdownTime"
+$vmsShutdown = Get-AzVM | Where-Object {$_.Tags.Keys -contains "ShutdownTime"}
+$vmsStartup = Get-AzVM | Where-Object {$_.Tags.Keys -contains "StartTime"}
+# Loop through each VM
+foreach ($vm in $vmsShutdown) {
+    # Get the value of the ShutdownTime tag
+    $shutdownTime = [DateTime]::ParseExact($vm.Tags["ShutdownTime"], "HH:mm", $null)
+    #$shutdownTime = $vm.Tags["ShutdownTime"]
+    
+    # Get the current time and format it to HH:mm
+    $currentTime = Get-Date
+    $timeDiff = ($currentTime - $shutdownTime).TotalMinutes
+    
+    # Compare the value of the tag with the current time
+    if ($timeDiff -le 80 -and $timeDiff -ge 0) {
+        # Shut down the VM
+        Stop-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Force
+        Write-Output "Shutting down VM $($vm.Name) in $($vm.ResourceGroupName) because ShutdownTime tag value is $($shutdownTime)."
+    }
+
+foreach ($vm in $vmsStartup) {
+    # Get the value of the ShutdownTime tag
+    $startupTime = [DateTime]::ParseExact($vm.Tags["StartTime"], "HH:mm", $null)
+    #$shutdownTime = $vm.Tags["ShutdownTime"]
+    
+    # Get the current time and format it to HH:mm
+    $currentTime = Get-Date
+    $timeDiff = ($currentTime - $startupTime).TotalMinutes
+    
+    # Compare the value of the tag with the current time
+    if ($timeDiff -le 60 -and $timeDiff -ge 0) {
+        # Shut down the VM
+        Start-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name
+        Write-Output "Starting VM $($vm.Name) in $($vm.ResourceGroupName) because StartupTime tag value is $($startupTime)."
+    }
+}
+}
